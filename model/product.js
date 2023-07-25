0b966ad51508f15f3e7b9c7ce813e19822f46396
harshraj29 <rajh3282@gmail.com>
2023-07-25 16:20:50 +0530

    Final

---
+++ model/product.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+//creating the product schema
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  quantity: Number,
+});
+//creting a new model called "product"
+
+const Product = mongoose.model("product", productSchema);
+
+module.exports = Product;
